2b807e2348c3b61748bce2172654275e9e6124a6
Sando George <sando.csc@gmail.com>
2017-01-30 08:31:07 +0100

    Can now push and pull profiles to and from the recommender system.

--- app/dash/profile_views.py
+++ app/dash/profile_views.py
@@ -1,13 +1,18 @@
 """Implements dashboard endpoints."""
 
 
+from bson.objectid import ObjectId
+
+
 from flask import (
     flash, redirect, render_template, url_for,
 )
 from flask_breadcrumbs import register_breadcrumb
 
 
-from app import sql
+from app import (
+    mongo, sql,
+)
 from app.confirm import confirm_required
 from app.dash import dash
 from app.dash.profile_forms import (
@@ -82,7 +87,7 @@ def profile(pid):
         'navwell': True,
         'page_header': 'Edit Profile',
         'form': form,
-        'pid': pid
+        'profile': profile
     }
     return render_template('dash/profiles/edit-profile.html', **page_vars)
 
@@ -101,3 +106,58 @@ def delete_profile(pid):
 
     flash('Profile successfully deleted.', 'success')
     return redirect(url_for('dash.profiles'))
+
+
+@dash.route('/profiles/<string:pid>/push')
+@admin_required
+@confirm_required(
+    'Are you sure you want to push this profile to the recommender system?',
+    'I am sure', 'dash.profile', ['pid'], 'danger'
+)
+def push_profile(pid):
+    """ Push a profile to the recommender system.
+        It will now be considered in recommendations for other profiles in
+        a sandbox setting.
+    """
+    profile = mongo.db.profiles
+    p = Profile.query.filter_by(id=pid).first()
+
+    insert_result = profile.insert_one({
+        'name': p.name,
+        'external_id': p.id
+    })
+
+    # Save the id given to the profile by the recommender system.
+    # Mark profile as being pushed to the recommender system.
+    p.rs_id = str(insert_result.inserted_id)
+    p.pushed = True
+    sql.session.add(p)
+    sql.session.commit()
+
+    flash('Profile successfully pushed.', 'success')
+    return redirect(url_for('dash.profile', pid=pid))
+
+
+@dash.route('/profiles/<string:pid>/pull')
+@admin_required
+@confirm_required(
+    'Are you sure you want to pull this profile from the recommender system?',
+    'I am sure', 'dash.profile', ['pid'], 'danger'
+)
+def pull_profile(pid):
+    """ Pull a profile from the recommender system.
+        It will no longer be considered in recommendations for profiles.
+    """
+    profile = mongo.db.profiles
+    p = Profile.query.filter_by(id=pid).first()
+
+    profile.delete_one({'_id': ObjectId(p.rs_id)})
+
+    # Clear pushed flag on profile.
+    p.rs_id = None
+    p.pushed = False
+    sql.session.add(p)
+    sql.session.commit()
+
+    flash('Profile successfully pulled.', 'success')
+    return redirect(url_for('dash.profile', pid=pid))
