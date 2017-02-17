7c1abb954dd11a998b3a64af1d092fb51860a584
Sando George <sando.csc@gmail.com>
2017-02-17 02:55:09 +0100

    Code refactor. Just making it easier to add conditional code for other endpoints.

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -2,65 +2,67 @@
 	var bc = $("body").attr('class').split(" ");
 
 	// Profile edit page.
-	if ($.inArray("bp-dash", bc) > -1 && $.inArray("ep-profile", bc) > -1 ) {
-		// Custom OAuth2 client.
-		var oauthClient = OAuth2Client.init({
-			client_id: FlaskData.oauth_client_id,
-			client_secret: FlaskData.oauth_client_secret,
-			access_token_url: Flask.url_for("oauth.access_token", {"_external": true}),
-			grant_type: "client_credentials"
-		});
-
-		// Kooyara Recommender System API.
-		var api = KAPI.init({
-			endpoints: {
-				get_inaction_statement: Flask.url_for(
-						"api_v1_0.get_inaction_statement",
-						{"_external": true, "_id": FlaskData.pid}),
-				post_reaction: Flask.url_for(
-						"api_v1_0.post_reaction", {"_external": true}),
-				get_matches: Flask.url_for(
-						"api_v1_0.get_matches",
-						{"_external": true, "_id": FlaskData.pid})
-			}
-		});
-		
-		// Slider.
-		var aSlider = $("#k-ua-slider");
-		aSlider.slider({
-			min: 1,
-			max: 5,
-			value: 3
-		}).slider("pips", {
-			rest: "label",
-			labels: [1, 2, 3, 4, 5]
-		});
-		
-		var submit = $("#k-ua-buttons .answer");
-		submit.on("click", function (e) {
-			data = {
-				profile: FlaskData.pid,
-				statement: FlaskData.statement,
-				reaction: aSlider.slider("option", "value")
-			}
+	if ($.inArray("bp-dash", bc) > -1) {
+		if ($.inArray("ep-profile", bc) > -1) {
+			// Custom OAuth2 client.
+			var oauthClient = OAuth2Client.init({
+				client_id: FlaskData.oauth_client_id,
+				client_secret: FlaskData.oauth_client_secret,
+				access_token_url: Flask.url_for("oauth.access_token", {"_external": true}),
+				grant_type: "client_credentials"
+			});
+	
+			// Kooyara Recommender System API.
+			var api = KAPI.init({
+				endpoints: {
+					get_inaction_statement: Flask.url_for(
+							"api_v1_0.get_inaction_statement",
+							{"_external": true, "_id": FlaskData.pid}),
+					post_reaction: Flask.url_for(
+							"api_v1_0.post_reaction", {"_external": true}),
+					get_matches: Flask.url_for(
+							"api_v1_0.get_matches",
+							{"_external": true, "_id": FlaskData.pid})
+				}
+			});
 			
-			postReaction(data);
-		});
-		
-		var skip = $("#k-ua-buttons .skip");
-		skip.on("click", function (e) {
-			data = {
-				profile: FlaskData.pid,
-				statement: FlaskData.statement,
-				reaction: -1
-			}
+			// Slider.
+			var aSlider = $("#k-ua-slider");
+			aSlider.slider({
+				min: 1,
+				max: 5,
+				value: 3
+			}).slider("pips", {
+				rest: "label",
+				labels: [1, 2, 3, 4, 5]
+			});
 			
-			postReaction(data);
-		});
-
-		if (FlaskData.pushed == "True") {
-			getInactionStatement();
-		}		
+			var submit = $("#k-ua-buttons .answer");
+			submit.on("click", function (e) {
+				data = {
+					profile: FlaskData.pid,
+					statement: FlaskData.statement,
+					reaction: aSlider.slider("option", "value")
+				}
+				
+				postReaction(data);
+			});
+			
+			var skip = $("#k-ua-buttons .skip");
+			skip.on("click", function (e) {
+				data = {
+					profile: FlaskData.pid,
+					statement: FlaskData.statement,
+					reaction: -1
+				}
+				
+				postReaction(data);
+			});
+	
+			if (FlaskData.pushed == "True") {
+				getInactionStatement();
+			}
+		}// end ep-profile.
 	}
 
 
